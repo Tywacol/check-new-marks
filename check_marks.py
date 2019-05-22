30824c57e6cb835ffa05794dea3a4d84ecdf37ec
Corto Callerisa <tylerdurdenleg@gmail.com>
2019-05-22 17:00:26 +0200

    merged debug into master, cleaned it

--- check_marks.py
+++ check_marks.py
@@ -6,15 +6,6 @@ from lxml import html
 from bs4 import BeautifulSoup
 from notify_run import Notify
 
-
-
-#Tres mauvais apparament
-#from importlib import reload
-#import sys
-#reload(sys)
-#sys.setdefaultencoding('utf8')
-
-
 USERNAME = "USERNAME"
 PASSWORD = "PASSWORD"
 
@@ -27,16 +18,14 @@ def send_mail():
 def main():
     session_requests = requests.session()
 
-    # Get login csrf token
     result = session_requests.get(LOGIN_URL)
     tree = html.fromstring(result.text)
-    #authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+    
 
     # Create payload
     payload = {
         "login": USERNAME, 
         "password": PASSWORD, 
-        #"csrfmiddlewaretoken": authenticity_token
     }
 
     # Perform login
@@ -44,20 +33,9 @@ def main():
 
     # Scrape url
     result = session_requests.get(URL, headers = dict(referer = URL))
-    #print("Result content : ")
-    #print(result.content)
-    tree = html.fromstring(result.content)
-    #print("tree : ")
-    #print(tree)
-    bucket_names = tree.xpath("//td")
-    #print("bucket_names : ")
 
-    #print(bucket_names)
-    #print("soup.prettify()")
     soup = BeautifulSoup(result.content, 'html.parser')
 
-    #print(soup.prettify())
-    #print()
     pair = True
 
     lines1 = []
@@ -65,15 +43,8 @@ def main():
     f_notes = open("notes.txt","r+")
     lines2 = f_notes.readlines()
 
-  
-    #print("len lines 2 = " + str(len(lines2)))
-    #print()
-    #print("lines2 = " + str(lines1))
-
     #check for emptyness (first run)
     if len(lines2) == 0 :
-        #print("len == 0")
-        #print("lines2 = " + str(lines1))
         for td in soup.find_all('td'):
                 if pair:
                         f_notes.write(td.contents[0] + " : ")
@@ -85,26 +56,14 @@ def main():
     for td in soup.find_all('td'):
         if pair:
                 s += str(td.contents[0]) + " : " 
-                 #print(td.contents[0] + " : ", end='')
-                #lines1.append(td.contents[0])
         else :
-                #print(td.contents[0])
                 s += td.contents[0] + '\n'
                 lines1.append(s)
                 s = ''
         pair = not(pair)
 
-    #could also use set intersection between lines 1 and 2 
-    # ex : value = { k : second_dict[k] for k in set(second_dict) - set(first_dict) }
-
-    #diff = difflib.unified_diff(lines2, lines1, fromfile='file1', tofile='file2', lineterm='', n=0)
-    #lines = list(diff)[2:]
-    #added = [line[1:] for line in lines if line[0] == '+']
-
     added = list(set(lines1) - set(lines2))
-    #print("added_set = " +str(added_set))
 
-    #removed = [line[1:] for line in lines if line[0] == '-']
     #print('additions:')
     print("lines1 : " +str(lines1))
     print("lines2 : " +str(lines1))
@@ -120,12 +79,7 @@ def main():
                 notify.send('Note : '+line.split(':')[0]) 
         f_notes.close()
 
-    #print("lines1 = " + str(lines1))
-    #print("lines2 = " + str(lines2))
-
     f_notes.close()
-
-
 
 if __name__ == '__main__':
     i = 1
