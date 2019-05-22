f37767e631639fb9e952e07ed398b806374f2e0a
Corto Callerisa <tylerdurdenleg@gmail.com>
2019-05-22 04:37:08 +0200

    Cleaned code

--- check_marks.py
+++ check_marks.py
@@ -14,21 +14,20 @@ LOGIN_URL = "https://portail.polytech-lille.fr/login/"
 URL = "http://appliportal.polytech-lille.fr/mypolytech/mesNotes.php"
 
 def send_mail():
-	pass
+	#TODO
+    pass
+
 
 def main():
     session_requests = requests.session()
 
-    # Get login csrf token
     result = session_requests.get(LOGIN_URL)
     tree = html.fromstring(result.text)
-    #authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
 
     # Create payload
     payload = {
         "login": USERNAME, 
         "password": PASSWORD, 
-        #"csrfmiddlewaretoken": authenticity_token
     }
 
     # Perform login
@@ -36,36 +35,19 @@ def main():
 
     # Scrape url
     result = session_requests.get(URL, headers = dict(referer = URL))
-    #print("Result content : ")
-    #print(result.content)
     tree = html.fromstring(result.content)
-    #print("tree : ")
-    #print(tree)
     bucket_names = tree.xpath("//td")
-    #print("bucket_names : ")
 
-    #print(bucket_names)
-    #print("soup.prettify()")
     soup = BeautifulSoup(result.content, 'html.parser')
 
-    #print(soup.prettify())
-    #print()
     pair = True
 
     lines1 = []
-
     f_notes = open("notes.txt","r+")
     lines2 = f_notes.readlines()
 
-  
-    #print("len lines 2 = " + str(len(lines2)))
-    #print()
-    #print("lines2 = " + str(lines1))
-
     #check for emptyness (first run)
     if len(lines2) == 0 :
-    	#print("len == 0")
-    	#print("lines2 = " + str(lines1))
     	for td in soup.find_all('td'):
 	    	if pair:
 	    		f_notes.write(td.contents[0] + " : ")
@@ -77,10 +59,7 @@ def main():
     for td in soup.find_all('td'):
     	if pair:
     		s += str(td.contents[0]) + " : " 
-    		 #print(td.contents[0] + " : ", end='')
-    		#lines1.append(td.contents[0])
     	else :
-    		#print(td.contents[0])
     		s += td.contents[0] + '\n'
     		lines1.append(s)
     		s = ''
@@ -92,21 +71,15 @@ def main():
     diff = difflib.unified_diff(lines2, lines1, fromfile='file1', tofile='file2', lineterm='', n=0)
     lines = list(diff)[2:]
     added = [line[1:] for line in lines if line[0] == '+']
-	#removed = [line[1:] for line in lines if line[0] == '-']
-    #print('additions:')
 
     if len(added) != 0 :
     	f_notes = open("notes.txt","a")
     	notify = Notify()
     	for line in added:
     		f_notes.write(str(line))
-    		#print(line)
     		notify.send(line) 
     	f_notes.close()
-
-    #print("lines1 = " + str(lines1))
-    #print("lines2 = " + str(lines2))
-
+    
     f_notes.close()
 
 
